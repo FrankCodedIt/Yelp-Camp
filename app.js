deda506e3a27ab3b762f99991e4e98cba4cabec6
Frank Ndlovu <franklyu6@gmail.com>
2025-05-08 15:44:42 +0200

    Update app.js & camgrounds index

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ app.get('/', (request, response) => {
     response.render('home')
 })
 
-//make new campground
-app.get('/makecampground', async (request, response) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
-    await camp.save();
-    response.send(camp);
-});
+
+app.get('/campgrounds', async (request, response) => {
+    const campgrounds = await Campground.find({});
+    response.render('campgrounds/index', {campgrounds})
+})
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
