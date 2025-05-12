d17528af650018666da0e69e40ff54f5f139c8f5
Frank Ndlovu <franklyu6@gmail.com>
2025-05-12 11:22:34 +0200

    Basic CRUD routers

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const Campground = require('./models/campground');
+const { request } = require('http');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -16,6 +18,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
 app.get('/', (request, response) => {
     response.render('home')
 })
@@ -26,6 +31,38 @@ app.get('/campgrounds', async (request, response) => {
     response.render('campgrounds/index', {campgrounds})
 })
 
+app.get('/campgrounds/new', (request, response) => {
+    response.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (request, response) => {
+    const campground = new Campground(request.body.campground)
+    await campground.save()
+    response.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+app.get('/campgrounds/:id', async (request, response) =>{
+    const campground = await Campground.findById(request.params.id)
+    response.render('campgrounds/show', {campground})
+})
+
+app.get('/campgrounds/:id/edit', async (request, response) => {
+    const campground = await Campground.findById(request.params.id)
+    response.render('campgrounds/edit', {campground})
+})
+
+app.put('/campgrounds/:id', async (request, response) => {
+    const {id} = request.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...request.body.campground})
+    response.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (request, response) =>{
+    const {id} = request.params;
+    await Campground.findByIdAndDelete(id);
+    response.redirect('/campgrounds')
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
