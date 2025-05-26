c69058f2ad42f2bdbdea5d443a0fa1b8f082fb00
Frank Ndlovu <franklyu6@gmail.com>
2025-05-26 15:57:20 +0200

    Refactor app.js: reorganize imports, enhance session and passport setup, and streamline route definitions

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -24,9 +28,6 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
-
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false,
@@ -37,17 +38,30 @@ const sessionConfig = {
         maxAge: 1000*60*60*24*7,
     }
 }
+
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((request, response, next) => {
+    response.locals.currentUser = request.user;
     response.locals.success = request.flash('success');
     response.locals.error = request.flash('error');
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewsRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all(/(.*)/, (request, response, next) => {
