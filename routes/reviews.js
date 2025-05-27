c759ef77acf4aca053b20559ccefe0ac1d800471
Frank Ndlovu <franklyu6@gmail.com>
2025-05-27 15:46:06 +0200

    Refactor review routes to use controller methods and add authentication checks

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,39 +1,11 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
+const reviews = require('../controllers/reviews');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('./middleware');
 
-const Campground = require('../models/campground');
-const Review = require('../models/review');
-const { reviewSchema } = require('../schemas.js');
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-const validateReview = (request, response, next) => {
-    const {error} = reviewSchema.validate(request.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }else{
-        next();
-    }
-}
-
-router.post('/', validateReview, catchAsync(async (request, response) => {
-    const campground = await Campground.findById(request.params.id);
-    const review = new Review(request.body.review)
-    campground.reviews.push(review)
-    await review.save();
-    await campground.save();
-    request.flash('success', 'Created New Review!');
-    response.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.delete('/:reviewId', catchAsync(async (request, response) =>{
-    const {id, reviewId} = request.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    request.flash('success', 'Successfully deleted review!');
-    response.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
