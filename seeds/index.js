530955e65f504a358b7bc31360c3a5b90f67896a
u18068635 <103001511+u18068635@users.noreply.github.com>
2025-05-29 01:21:51 +0200

    Refactor seedDB function for improved readability and consistency

--- seeds/index.js
+++ seeds/index.js
@@ -1,39 +1,49 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campground');
-const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database Connected");
+  console.log("Database Connected");
 });
 
-
 //picking a random element from array
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 //seeding the database with 50 different combinations of camps genereated randomly using ciities
-const seedDB = async() =>{
-    await Campground.deleteMany({});
-    //generating the title of the campground using seedHelpers
-    for(let i = 0; i < 50; i++){
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() *20) + 10;
-        const camp = new Campground({
-            author: '68344bb6268cf0c4040d3b90', 
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nostrum voluptatum quam fuga molestiae atque repellendus exercitationem neque dignissimos distinctio ut, eius numquam quae ratione quasi dolorum laborum facere optio!',
-            price
-        })
-        await camp.save();
-    }
-}
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  //generating the title of the campground using seedHelpers
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "683765a3da4b66d666beced5",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      // image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nostrum voluptatum quam fuga molestiae atque repellendus exercitationem neque dignissimos distinctio ut, eius numquam quae ratione quasi dolorum laborum facere optio!",
+      price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dc05shrl6/image/upload/v1748472495/YelpCamp/s4revsm32ou8v7yfykv8.jpg",
+          filename: "YelpCamp/s4revsm32ou8v7yfykv8",
+        },
+        {
+          url: "https://res.cloudinary.com/dc05shrl6/image/upload/v1748472495/YelpCamp/nvbwi5wlmmzfejqqaakk.jpg",
+          filename: "YelpCamp/nvbwi5wlmmzfejqqaakk",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
 
-seedDB().then(()=> {
-    mongoose.connection.close();
-})
+seedDB().then(() => {
+  mongoose.connection.close();
+});
