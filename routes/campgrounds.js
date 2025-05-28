901aa8557d25fa5c79e50db762124badcb6ddf8b
u18068635 <103001511+u18068635@users.noreply.github.com>
2025-05-29 01:21:40 +0200

    Refactor campground routes for improved consistency and readability

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,31 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const catchAsync = require('../utils/catchAsync');
-const multer = require('multer');
-const { storage } = require('../cloudinary');
+const catchAsync = require("../utils/catchAsync");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
 const upload = multer({ storage });
 
-const Campground = require('../models/campground');
-const { isLoggedIn, isAuthor, validateCampground }  = require('../routes/middleware')
+const Campground = require("../models/campground");
+const {
+  isLoggedIn,
+  isAuthor,
+  validateCampground,
+} = require("../routes/middleware");
 
-const campgrounds = require('../controllers/campgrounds');
+const campgrounds = require("../controllers/campgrounds");
 
-router.get('/', catchAsync(campgrounds.index));
+router.get("/", catchAsync(campgrounds.index));
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
-// router.post('/', upload.array('image'), (req, res) => {
-//     console.log(req.body, req.files);
-//     res.send("It worked!")
-// })
+router.post(
+  "/",
+  isLoggedIn,
+  upload.array("image"),
+  validateCampground,
+  catchAsync(campgrounds.createCampground)
+);
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.get("/:id", catchAsync(campgrounds.showCampground));
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(campgrounds.renderEditForm)
+);
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground,  catchAsync(campgrounds.updateCampground));
+router.put(
+  "/:id",
+  isLoggedIn,
+  isAuthor,
+  validateCampground,
+  catchAsync(campgrounds.updateCampground)
+);
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+router.delete(
+  "/:id",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(campgrounds.deleteCampground)
+);
 
 module.exports = router;
