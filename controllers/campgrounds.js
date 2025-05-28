45cd885bcf379ca529f926e6dd1d1c05fddb13c1
u18068635 <103001511+u18068635@users.noreply.github.com>
2025-05-29 01:21:28 +0200

    Refactor campground controller for improved readability and consistency

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,60 +1,77 @@
-const Campground = require('../models/campground');
+const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (request, response) => {
-    const campgrounds = await Campground.find({});
-    response.render('campgrounds', {campgrounds})
-}
+  const campgrounds = await Campground.find({});
+  response.render("campgrounds", { campgrounds });
+};
 
 module.exports.renderNewForm = (request, response) => {
-    response.render('campgrounds/new')
-}
+  response.render("campgrounds/new");
+};
 
 module.exports.createCampground = async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    campground.author = req.user._id;
-    await campground.save();
-    console.log(campground);
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
-}
+  //   const geoData = await geocoder
+  //     .forwardGeocode({
+  //       query: "Johannesburg, SA",
+  //       limit: 1,
+  //     })
+  //     .send();
+  //   console.log(geoData.body.features);
+  //   res.send("Ok!!!!!");
+  const campground = new Campground(req.body.campground);
+  console.log(campground);
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.author = req.user._id;
+  await campground.save();
+  console.log(campground);
+  req.flash("success", "Successfully made a new campground!");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
-module.exports.showCampground = async (request, response) =>{
-    const campground = await Campground.findById(request.params.id)
+module.exports.showCampground = async (request, response) => {
+  const campground = await Campground.findById(request.params.id)
     .populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
     })
-    .populate('author');
-    if(!campground){
-        request.flash('error', 'Cannot find that campground!');
-        return response.redirect('/campgrounds');
-    }
-    response.render('campgrounds/show', {campground})
-}
+    .populate("author");
+  if (!campground) {
+    request.flash("error", "Cannot find that campground!");
+    return response.redirect("/campgrounds");
+  }
+  response.render("campgrounds/show", { campground });
+};
 
 module.exports.renderEditForm = async (request, response) => {
-    const {id} = request.params;
-    const campground = await Campground.findById(id);
-    if(!campground){
-        request.flash('error', 'Cannot find that campground!');
-        return response.redirect('/campgrounds');
-    }
-    response.render('campgrounds/edit', {campground})
-}
+  const { id } = request.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    request.flash("error", "Cannot find that campground!");
+    return response.redirect("/campgrounds");
+  }
+  response.render("campgrounds/edit", { campground });
+};
 
 module.exports.updateCampground = async (request, response) => {
-    const {id} = request.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...request.body.campground})
-    request.flash('success', 'Successfully updated campground!');
-    response.redirect(`/campgrounds/${campground._id}`)
-}
+  const { id } = request.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...request.body.campground,
+  });
+  request.flash("success", "Successfully updated campground!");
+  response.redirect(`/campgrounds/${campground._id}`);
+};
 
-module.exports.deleteCampground = async (request, response) =>{
-    const {id} = request.params;
-    await Campground.findByIdAndDelete(id);
-    request.flash('success', 'Successfully deleted campground!');
-    response.redirect('/campgrounds')
-}
+module.exports.deleteCampground = async (request, response) => {
+  const { id } = request.params;
+  await Campground.findByIdAndDelete(id);
+  request.flash("success", "Successfully deleted campground!");
+  response.redirect("/campgrounds");
+};
