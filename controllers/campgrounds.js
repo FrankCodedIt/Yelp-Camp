88f099f31d0118e6a7edbea76d87297197f2ab5c
FrankCodedIt <franklyu6@gmail.com>
2025-05-29 19:13:06 +0200

    Refactor createCampground function for improved readability and consistency

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -13,16 +13,14 @@ module.exports.renderNewForm = (request, response) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
-  //   const geoData = await geocoder
-  //     .forwardGeocode({
-  //       query: "Johannesburg, SA",
-  //       limit: 1,
-  //     })
-  //     .send();
-  //   console.log(geoData.body.features);
-  //   res.send("Ok!!!!!");
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
   const campground = new Campground(req.body.campground);
-  console.log(campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
